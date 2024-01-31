927ef47eccf9b80c60d1ac53df40840e3c530668
Yiim <ilyimyan@gmail.com>
2024-01-31 22:37:04 +0800

    initial commit

---
+++ javascript/8_Kyu/reversed_string.js
@@ -0,0 +1,9 @@
+function solution(str){
+    return str.split('').reverse().join('');
+}
+
+console.log(solution('wordl'));
+console.log(solution('hello'));
+console.log(solution(''));
+console.log(solution('h'));
+
